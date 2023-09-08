44523ea6a8e87f20cb6cf99f9d5a2849e07e239a
huggo <victorhuggocontato42@gmail.com>
2023-09-08 02:48:31 -0300

    feat(aula1): code repo
    - https://github.com/rodrigobranas/cccat13_1

--- ccca13/aula1_2023-09-04/code/cccat13_1/backend/ride/src/CpfValidator.ts
+++ ccca13/aula1_2023-09-04/code/cccat13_1/backend/ride/src/CpfValidator.ts
@@ -1,42 +1,42 @@
 export default class CpfValidator {
 
-	validate (cpf: string) {
-		if (!cpf) return false;
-		cpf = this.clean(cpf); 
-		if (this.isInvalidLength(cpf)) return false;
-		if (this.allDigitsTheSame(cpf)) {
-		const dg1 = this.calculateDigit(cpf, 10); 
-		const dg2 = this.calculateDigit(cpf, 11);
-		let checkDigit = this.extractDigit(cpf);  
-		const calculatedDigit = `${dg1}${dg2}`;  
-		return checkDigit === calculatedDigit;
-			
-		} else return false
-	}
+    validate(cpf: string) {
+        if (!cpf) return false;
+        cpf = this.clean(cpf);
+        if (this.isInvalidLength(cpf)) return false;
+        if (this.allDigitsTheSame(cpf)) {
+            const dg1 = this.calculateDigit(cpf, 10);
+            const dg2 = this.calculateDigit(cpf, 11);
+            let checkDigit = this.extractDigit(cpf);
+            const calculatedDigit = `${dg1}${dg2}`;
+            return checkDigit === calculatedDigit;
 
-	clean (cpf: string) {
-		return cpf.replace(/\D/g, "");
-	}
+        } else return false
+    }
 
-	isInvalidLength (cpf: string) {
-		return cpf.length !== 11;
-	}
+    clean(cpf: string) {
+        return cpf.replace(/\D/g, "");
+    }
 
-	allDigitsTheSame (cpf: string) {
-		return !cpf.split("").every(c => c === cpf[0]);
-	}
+    isInvalidLength(cpf: string) {
+        return cpf.length !== 11;
+    }
 
-	calculateDigit (cpf: string, factor: number) {
-		let total = 0;
-		for (const digit of cpf) {
-			if (factor > 1) total += parseInt(digit) * factor--;
-		}
-		const rest = total%11;
-		return (rest < 2) ? 0 : 11 - rest;
-	}
+    allDigitsTheSame(cpf: string) {
+        return !cpf.split("").every(c => c === cpf[0]);
+    }
 
-	extractDigit (cpf: string) {
-		return cpf.substring(cpf.length-2, cpf.length);
-	}
+    calculateDigit(cpf: string, factor: number) {
+        let total = 0;
+        for (const digit of cpf) {
+            if (factor > 1) total += parseInt(digit) * factor--;
+        }
+        const rest = total % 11;
+        return (rest < 2) ? 0 : 11 - rest;
+    }
+
+    extractDigit(cpf: string) {
+        return cpf.substring(cpf.length - 2, cpf.length);
+    }
 
 }
